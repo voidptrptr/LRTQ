bfc852b6b1c8c58fce4dd9df7ee63fc8e0a98cce
voidptrptr <75911458+voidptrptr@users.noreply.github.com>
2022-12-19 20:58:55 +0000

    Sphere intersection

--- LRTQ/main.cpp
+++ LRTQ/main.cpp
@@ -8,11 +8,31 @@
 #include <iostream>
 
 color ray_colour(const ray& r) {
+	
+	// If sphere_hit > 0, colour using colour vector
+	
+	if (sphere_hit(point3(0, 0, -1), 0.5, r))
+		return color(0, 1, 0);
+	
 	vec3 unit_dir = unit_vector(r.direction());
 	auto t = 0.5 * (unit_dir.y() + 1.0);
 	return(1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
+bool sphere_hit(const point3& center, double radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	
+	// A = direction squared
+	auto a = dot(r.direction(), r.direction());
+
+	auto b = 2.0 * dot(oc, r.direction());
+
+	// C = OC squared - radius squared
+	auto c = dot(oc, oc) - radius * radius;
+
+	auto discriminant = b * b - (4 * a * c);
+	return discriminant > 0;
+}
 
 int main() {
 
@@ -37,7 +57,7 @@ int main() {
 	std::cout << "P3\n" << width << ' ' << height << "\n255\n";
 
 	//Write image data to buffer
-	for (int j = height - 1; j > 0; --j) {
+	for (int j = height - 1; j >= 0; --j) {
 		std::cerr << "\rLines Left to render" << j << "" << std::flush;
 		for (int i = 0; i < width; ++i) {
 			auto u = double(i) / (width - 1);
