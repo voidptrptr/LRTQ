c437090223c354487dfdb8a9e6f72a8100430653
voidptrptr <75911458+voidptrptr@users.noreply.github.com>
2022-12-19 20:31:09 +0000

    Viewport consts and background function

--- LRTQ/main.cpp
+++ LRTQ/main.cpp
@@ -7,10 +7,31 @@
 
 #include <iostream>
 
+color ray_colour(const ray& r) {
+	vec3 unit_dir = unit_vector(r.direction());
+	auto t = 0.5 * (unit_dir.y() + 1.0);
+	return(1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+
 int main() {
+
+
+	//Consts for camera
+	const auto aspect_ratio = 16 / 9;
+
 	// Define constants for image size
-	const int width = 256;
-	const int height = 256;
+	const int width = 400;
+	const int height = static_cast<int>(width / aspect_ratio);
+
+	//viewport
+	const auto view_height = 2.0;
+	const int view_width = view_height * aspect_ratio;
+	const auto focal_length = 1;
+	auto origin = point3(0, 0, 0);
+	auto horizontal = vec3(view_width, 0, 0);
+	auto vertical = vec3(0, view_height, 0);
+	auto lower_left = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
 	//Init ppm 
 	std::cout << "P3\n" << width << ' ' << height << "\n255\n";
